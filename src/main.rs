50856d44594e80fb22eb3e5114230aadc4e710d9
Blechlawine <marczinser@gmx.de>
2023-03-07 22:13:15 +0100

    feat: add button to open source directory picker

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use gtk::{glib, Application};
-use gtk::{prelude::*, ApplicationWindow};
+use gtk::{glib, Application, Button};
+use gtk::{prelude::*, ApplicationWindow, FileChooserDialog};
 
 const APP_ID: &str = "de.zmarc.photosync";
 
@@ -12,9 +12,38 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let file_chooser = FileChooserDialog::builder()
+        .title("Select source directory")
+        .action(gtk::FileChooserAction::SelectFolder)
+        .build();
+
+    file_chooser.add_button("Select", gtk::ResponseType::Accept);
+
+    file_chooser.connect_response(move |dialog, response| {
+        if response == gtk::ResponseType::Accept {
+            if let Some(file) = dialog.file() {
+                dbg!(file.path());
+                dialog.destroy();
+            }
+        }
+    });
+
+    let btn_open_source_picker = Button::builder()
+        .label("Select source directory")
+        .margin_top(10)
+        .margin_bottom(10)
+        .margin_start(10)
+        .margin_end(10)
+        .build();
+
+    btn_open_source_picker.connect_clicked(move |_| {
+        file_chooser.show();
+    });
+
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Photosync")
+        .child(&btn_open_source_picker)
         .build();
 
     window.present();
