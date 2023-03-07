31f3896c69883bfea105bffcbd09ae1248cef4ae
Blechlawine <marczinser@gmx.de>
2023-03-07 22:31:08 +0100

    feat: add label to show selected path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use gtk::{glib, Application, Button};
+use gtk::{glib, Application, Button, Label, Box, Orientation};
 use gtk::{prelude::*, ApplicationWindow, FileChooserDialog};
 
 const APP_ID: &str = "de.zmarc.photosync";
@@ -17,17 +17,6 @@ fn build_ui(app: &Application) {
         .action(gtk::FileChooserAction::SelectFolder)
         .build();
 
-    file_chooser.add_button("Select", gtk::ResponseType::Accept);
-
-    file_chooser.connect_response(move |dialog, response| {
-        if response == gtk::ResponseType::Accept {
-            if let Some(file) = dialog.file() {
-                dbg!(file.path());
-                dialog.destroy();
-            }
-        }
-    });
-
     let btn_open_source_picker = Button::builder()
         .label("Select source directory")
         .margin_top(10)
@@ -36,6 +25,29 @@ fn build_ui(app: &Application) {
         .margin_end(10)
         .build();
 
+    let lbl_source_path = Label::builder()
+        .label("No source directory selected")
+        .margin_top(10)
+        .margin_bottom(10)
+        .margin_start(10)
+        .margin_end(10)
+        .build();
+
+    let box_container = Box::new(Orientation::Vertical, 0);
+    box_container.append(&btn_open_source_picker);
+    box_container.append(&lbl_source_path);
+
+    file_chooser.add_button("Select", gtk::ResponseType::Accept);
+
+    file_chooser.connect_response(move |dialog, response| {
+        if response == gtk::ResponseType::Accept {
+            if let Some(file) = dialog.file() {
+                lbl_source_path.set_label(&file.path().unwrap().to_string_lossy());
+                dialog.destroy();
+            }
+        }
+    });
+
     btn_open_source_picker.connect_clicked(move |_| {
         file_chooser.show();
     });
@@ -43,7 +55,7 @@ fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Photosync")
-        .child(&btn_open_source_picker)
+        .child(&box_container)
         .build();
 
     window.present();
