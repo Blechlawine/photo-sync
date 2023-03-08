eb6969646a6d077fa21bbdf408c1f37cbd45d48e
Blechlawine <marczinser@gmx.de>
2023-03-08 18:32:11 +0100

    feat: add destination picker

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,43 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let file_chooser = FileChooserDialog::builder()
+    let source_path_chooser = FileChooserDialog::builder()
         .title("Select source directory")
         .action(gtk::FileChooserAction::SelectFolder)
         .build();
 
-    let btn_open_source_picker = Button::builder()
-        .label("Select source directory")
-        .margin_top(10)
-        .margin_bottom(10)
-        .margin_start(10)
-        .margin_end(10)
+    let destination_path_chooser = FileChooserDialog::builder()
+        .title("Select destination directory")
+        .action(gtk::FileChooserAction::SelectFolder)
         .build();
+
+    let btn_open_source_picker = Button::builder().label("Select source directory").build();
 
     let lbl_source_path = Label::builder()
         .label("No source directory selected")
-        .margin_top(10)
-        .margin_bottom(10)
-        .margin_start(10)
-        .margin_end(10)
         .build();
 
-    let box_container = Box::new(Orientation::Vertical, 0);
+    let btn_open_destination_picker = Button::builder()
+        .label("Select destination directory")
+        .build();
+
+    let lbl_destination_path = Label::builder()
+        .label("No destination directory selected")
+        .build();
+
+    let box_container = Box::new(Orientation::Vertical, 10);
+    box_container.set_margin_bottom(10);
+    box_container.set_margin_top(10);
+    box_container.set_margin_start(10);
+    box_container.set_margin_end(10);
     box_container.append(&btn_open_source_picker);
     box_container.append(&lbl_source_path);
+    box_container.append(&btn_open_destination_picker);
+    box_container.append(&lbl_destination_path);
 
-    file_chooser.add_button("Select", gtk::ResponseType::Accept);
+    source_path_chooser.add_button("Select", gtk::ResponseType::Accept);
 
-    file_chooser.connect_response(move |dialog, response| {
+    source_path_chooser.connect_response(move |dialog, response| {
         if response == gtk::ResponseType::Accept {
             if let Some(file) = dialog.file() {
                 lbl_source_path.set_label(&file.path().unwrap().to_string_lossy());
@@ -48,8 +57,23 @@ fn build_ui(app: &Application) {
         }
     });
 
+    destination_path_chooser.add_button("Select", gtk::ResponseType::Accept);
+
+    destination_path_chooser.connect_response(move |dialog, response| {
+        if response == gtk::ResponseType::Accept {
+            if let Some(file) = dialog.file() {
+                lbl_destination_path.set_label(&file.path().unwrap().to_string_lossy());
+                dialog.destroy();
+            }
+        }
+    });
+
     btn_open_source_picker.connect_clicked(move |_| {
-        file_chooser.show();
+        source_path_chooser.show();
+    });
+
+    btn_open_destination_picker.connect_clicked(move |_| {
+        destination_path_chooser.show();
     });
 
     let window = ApplicationWindow::builder()
